3c1e14fc5962457e1fb0940db69f58cd9a2a4a4b
dunglv <dunglvdeveloper@gmail.com>
2024-12-29 18:19:30 +0700

    update code video 45

--- src/pages/DetailProduct/index.jsx
+++ src/pages/DetailProduct/index.jsx
@@ -1,5 +1,97 @@
+import MyHeader from '@components/Header/Header';
+import MainLayout from '@components/Layout/Layout';
+import styles from './styles.module.scss';
+import Button from '@components/Button/Button';
+
 function DetailProduct() {
-    return <div>detail Product</div>;
+    const {
+        container,
+        navigateSection,
+        contentSection,
+        price,
+        imageBox,
+        infoBox,
+        description,
+        boxSize,
+        size,
+        titleSize,
+        functionInfo,
+        boxBtn,
+        incrementAmount,
+        orSection
+    } = styles;
+    return (
+        <div>
+            <MyHeader />
+            <div className={container}>
+                <MainLayout>
+                    <div className={navigateSection}>
+                        <div>Home {'>'} Men</div>
+                        <div className='' style={{ cursor: 'pointer' }}>
+                            {'<'} Return to previous page{' '}
+                        </div>
+                    </div>
+
+                    <div className={contentSection}>
+                        <div className={imageBox}>
+                            <img
+                                src='https://xstore.b-cdn.net/elementor2/marseille04/wp-content/uploads/sites/2/2022/12/Image-7.3-min.jpg'
+                                alt='zxczx'
+                            />
+
+                            <img
+                                src='https://xstore.b-cdn.net/elementor2/marseille04/wp-content/uploads/sites/2/2022/12/Image-7.3-min.jpg'
+                                alt='zxczx'
+                            />
+
+                            <img
+                                src='https://xstore.b-cdn.net/elementor2/marseille04/wp-content/uploads/sites/2/2022/12/Image-7.3-min.jpg'
+                                alt='zxczx'
+                            />
+
+                            <img
+                                src='https://xstore.b-cdn.net/elementor2/marseille04/wp-content/uploads/sites/2/2022/12/Image-7.3-min.jpg'
+                                alt='zxczx'
+                            />
+                        </div>
+                        <div className={infoBox}>
+                            <h1>Title Product</h1>
+                            <p className={price}>$1,879.99</p>
+                            <p className={description}>
+                                Amet, elit tellus, nisi odio velit ut. Euismod
+                                sit arcu, quisque arcu purus orci leo.
+                            </p>
+
+                            <p className={titleSize}>Size</p>
+                            <div className={boxSize}>
+                                <div className={size}>L</div>
+                                <div className={size}>M</div>
+                                <div className={size}>S</div>
+                            </div>
+
+                            <div className={functionInfo}>
+                                <div className={incrementAmount}>
+                                    <div>-</div>
+                                    <div>1</div>
+                                    <div>+</div>
+                                </div>
+
+                                <div className={boxBtn}>
+                                    <Button content={'Add to cart'} />
+                                </div>
+                            </div>
+
+                            <div className={orSection}>
+                                <div></div>
+                                <span>OR</span>
+                                <div></div>
+                            </div>
+                        </div>
+                    </div>
+                </MainLayout>
+            </div>
+        </div>
+    );
 }
 
 export default DetailProduct;
