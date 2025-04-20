027cdfc3362d6c17a920ba0eb8e22094e143f113
dunglv <dunglvdeveloper@gmail.com>
2025-04-20 11:14:15 +0700

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,6 +12,7 @@ import { useState } from 'react';
 import classNames from 'classnames';
 import { useContext } from 'react';
 import { SideBarContext } from '@/contexts/SideBarProvider';
+import { StoreContext } from '@/contexts/storeProvider';
 
 function MyHeader() {
     const {
@@ -35,16 +36,25 @@ function MyHeader() {
         userId,
         handleGetListProductsCart
     } = useContext(SideBarContext);
+    const { userInfo } = useContext(StoreContext);
 
     const handleOpenSideBar = (type) => {
         setIsOpen(true);
         setType(type);
     };
 
+    console.log(userInfo);
+
     const handleOpenCartSideBar = () => {
         handleGetListProductsCart(userId, 'cart');
         handleOpenSideBar('cart');
     };
+
+    const totalItemCart = listProductCart.length
+        ? listProductCart.reduce((acc, item) => {
+              return (acc += item.quantity);
+          }, 0)
+        : 0;
 
     useEffect(() => {
         setFixedPosition(scrollPosition > 80);
@@ -114,7 +124,7 @@ function MyHeader() {
                             />
 
                             <div className={quantity}>
-                                {listProductCart.length}
+                                {totalItemCart || userInfo?.amountCart || 0}
                             </div>
                         </div>
                     </div>
